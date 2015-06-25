05c994ea048822d4d8bd558128b12189718b48dc
dieGO <dgonzalez.BZ@gmail.com>
2015-06-25 20:13:25 +0200

    Actualización de mantenimiento. Limpieza del código realizada y organizados/añadidos comentarios de "--DEBUG" con dicho tag. Añadido ".gitignore" al repsitorio con exclusiones estándar para proyecto basado en LUA y IntelliJ.

--- init.lua
+++ init.lua
@@ -1,59 +1,61 @@
 conTimeout = 10 -- timeout antes de cambiar a modo AP
 
 timeoutCount = 0
+
 function netStatus()
-  timeoutCount = timeoutCount + 1
-  local s=wifi.sta.status()
-  if(s==5) then -- conectado, lanzamos la smApp
-	print('Conectado correctamente - (status 5)')
-    smartideaApp()
-    return
-  elseif(s==2 or s==3 or s==4) then -- conexion fallida, cambiamos a modo AP
-	print('Fallo al conectar - (status 2/3/4)')
-    confConn()
-    return
-  end
-  if(timeoutCount >= conTimeout) then -- agotado el tiempo cambiamos a modo AP
-	print('Timeout al conectar!')
-    confConn()
-    return
-  end
+    timeoutCount = timeoutCount + 1
+    local s=wifi.sta.status()
+    if(s==5) then -- conectado, lanzamos la smApp
+        local ip = wifi.sta.getip()
+        print('Conectado correctamente - (status 5)') --DEBUG
+        print('\n\nDireccion IP: ' .. ip) --DEBUG
+        smartideaApp()
+        return
+    elseif(s==2 or s==3 or s==4) then -- conexion fallida, cambiamos a modo AP
+        print('Fallo al conectar - (status 2/3/4)') --DEBUG
+        confConn()
+        return
+    end
+    if(timeoutCount >= conTimeout) then -- agotado el tiempo cambiamos a modo AP
+        print('Timeout al conectar!') --DEBUG
+        confConn()
+        return
+    end
 end
 
 function smartideaApp()
-  ip = wifi.sta.getip()
-  print('Conectado correctamente a la red WiFi con IP: ' .. ip)
-  doCleanup()
-  dofile('smDimmer.lua')
+    doCleanup()
+    dofile('smDimmer.lua')
 end
 
 function confConn()
-  print('Red WiFi no conectada/encontrada, lanzando modo AP ')
-  doCleanup()
-  dofile('smConfConn.lua') -- red no conectada/encontrada, cambiando a modo AP
+    print('Red WiFi no conectada/encontrada, lanzando modo AP ') --DEBUG
+    doCleanup()
+    -- red no conectada/encontrada, cambiando a modo AP
+    dofile('smConfConn.lua')
 end
 
 function doCleanup()
-  -- paramos el timer
-  tmr.stop(0)
-  -- reseteamos con nil las variables globales
-  conTimeout = nil
-  timeoutCount = nil
-  -- reseteamos con nil las funciones definidas
-  netStatus = nil
-  smartideaApp = nil
-  confConn = nil
-  ip = nil
-  doCleanup = nil
-  -- llamamos al recolector de basura
-  collectgarbage()
-  -- hacemos un delay para esperar a que se libere memoria
-  tmr.delay(5000)
+    print("Sacando la basura...") --DEBUG
+    -- paramos el timer
+    tmr.stop(0)
+    -- reseteamos con nil las variables globales
+    conTimeout = nil
+    timeoutCount = nil
+    -- reseteamos con nil las funciones definidas
+    netStatus = nil
+    smartideaApp = nil
+    confConn = nil
+    doCleanup = nil
+    -- llamamos al recolector de basura
+    collectgarbage()
+    -- hacemos un delay (ms) para esperar a que se libere memoria
+    tmr.delay(3000)
 end
 
--- establecemos modo cliente e intentamos autoconectar
+-- Establecemos modo cliente e intentamos autoconectar
 wifi.setmode(wifi.STATION)
 wifi.sta.autoconnect(1)
 
--- comprobamos el estado de la red cada segundo
+-- Comprobamos el estado de la red cada segundo
 tmr.alarm(0, 1000, 1, netStatus)
--- smDimmer.lua
+++ smDimmer.lua
@@ -1,18 +1,19 @@
-function incConnDimmer(conn) -- devolvemos una respuesta
-    print ("\n\nConexion HTTP recibida!")
+print("SmartApp iniciada correctamente") --DEBUG
+function incConnDimmer(conn)
+    print ("\n\nConexion HTTP recibida!") --DEBUG
     conn:send('HTTP/1.1 200 OK\n\n')
     conn:send('<!DOCTYPE HTML>\n<html>\n<head><meta content="text/html; charset=utf-8">\n<title>SMARTIDEA - SmartDimmer</title></head>\n<body>\n\n')
     conn:send('\n<h1>SmartDimmer</h1>')
     conn:send('\n\n\n<br />Conectado correctamente a la red WiFi')
-    ip = wifi.sta.getip()
+    local ip = wifi.sta.getip()
     conn:send('<br />Direccion IP:' .. ip)
-    ip = nil
     conn:send('\n</body></html>')
 end
 
-srv=net.createServer(net.TCP) -- arrancamos servidor web
+srv=net.createServer(net.TCP)
+-- arrancamos servidor web y procesamos las peticiones
 srv:listen(80,function(sock)
-    sock:on("receive", incConnDimmer) -- procesamos las peticiones
+    sock:on("receive", incConnDimmer)
     sock:on("sent", function(sock) 
         sock:close()
     end)
