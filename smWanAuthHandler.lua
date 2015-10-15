355e99365aaab8dd0fb242aa1d123ce68dfc7989
dieGO <dgonzalez.BZ@gmail.com>
2015-10-15 20:15:38 +0200

    Añadido callback en las funciones de envío de datos para enviar "\n" a modo de delimitador.

--- smWanAuthHandler.lua
+++ smWanAuthHandler.lua
@@ -5,7 +5,11 @@ function wanAuthHandler.authRequest (smSrv)
     local wanAuthRequest = {}
     wanAuthRequest["action"] = "DEVICE_AUTH"
     wanAuthRequest["id"] = "100999"
-    smSrv:send(cjson.encode(wanAuthRequest))
+    smSrv:send(cjson.encode(wanAuthRequest),
+        function()
+            smSrv:send("\n")
+        end
+    )
 end
 
 return wanAuthHandler
