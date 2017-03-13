1b6c897a9c709e33d59231b6fae4bb07b21bd7b1
Diego González <dgonzalez.bz@gmail.com>
2017-03-13 19:56:35 +0000

    smConfConn.lua translated to english. Removed smartidea mentions.

--- smConfConn.lua
+++ smConfConn.lua
@@ -1,6 +1,6 @@
-print("\nsmConfConn iniciada correctamente") --DEBUG
+print("\nsmConfConn successfully started") --DEBUG
 
-dofile("smAPConfData.lua") -- Set Station-AP mode and create SMARTIDEA-XX-YY WLAN
+dofile("smAPConfData.lua") -- Set Station-AP mode and create SMART-XX-YY WLAN
 
 refreshTimeout = 15 -- Timeout for AP scan
 availableAPs = {}
@@ -17,19 +17,19 @@ end
 
 function getAPs()
     print(collectgarbage("count")* 1024 .. "KB") -- Show used memory in KB
-    print ("Buscando APs...") --DEBUG
+    print ("Searching for APs...") --DEBUG
     wifi.sta.getap(1, getAPs_callback)
 end
 
 function sendAPlistPage(conn)
-    print ("\n\nEnviando web de configuracion!") --DEBUG
-    local webPage = 'HTTP/1.1 200 OK\n<!DOCTYPE HTML>\n<html><head><meta content="text/html; charset=utf-8">\n<title>SmartWiFi</title></head>\n<body>\n<form action="/" method="POST">\n<br/>\nSelecciona tu WLAN:<br/><br/>\n'
+    print ("\n\nSending WiFi setup webpage...") --DEBUG
+    local webPage = 'HTTP/1.1 200 OK\n<!DOCTYPE HTML>\n<html><head><meta content="text/html; charset=utf-8">\n<title>SmartWiFi</title></head>\n<body>\n<form action="/" method="POST">\n<br/>\nSelect your WLAN:<br/><br/>\n'
     for ap,v in pairs(availableAPs) do
         local ssid, rssi, authmode, channel = string.match(v, "([^,]+),([^,]+),([^,]+),([^,]+)")
-        webPage = webPage..'<input type="button" onClick=\'document.getElementById("ssid").value = "'..ssid..'"\' value="'..ssid..'"/><br/>Potencia: '..rssi..' Canal: '..channel..' Seguridad: '..authmode..' MAC: '..ap..'<br/>\n'
+        webPage = webPage..'<input type="button" onClick=\'document.getElementById("ssid").value = "'..ssid..'"\' value="'..ssid..'"/><br/>Power: '..rssi..' Channel: '..channel..' Security: '..authmode..' MAC: '..ap..'<br/>\n'
     end
     webPage = webPage..'SSID:<input type="text" id="ssid" name="ssid" value=""><br/>\nPassword:<input type="text" name="passwd" value=""><br/>\n'
-    webPage = webPage..'<input type="button" onClick="window.location.reload()" value="Refrescar"/>\n<input type="submit" name="reboot" value="Conectar"/>\n</form>\n</body>\n</html>'
+    webPage = webPage..'<input type="button" onClick="window.location.reload()" value="Refresh"/>\n<input type="submit" name="reboot" value="Connect"/>\n</form>\n</body>\n</html>'
     conn:send(webPage)
     conn:close()
     webPage = nil
@@ -44,14 +44,14 @@ function url_decode(str)
 end
 
 function incomingConnection(conn, payload)
-    print ("\n\nConexion HTTP recibida!") --DEBUG
+    print ("\n\nHTTP Connection received!") --DEBUG
     if (string.find(payload, "GET /favicon.ico HTTP/1.1") ~= nil) then
-        print("\nGET favicon solicitado") --DEBUG
+        print("\nGET favicon requested") --DEBUG
     elseif (string.find(payload, "GET / HTTP/1.1") ~= nil) then
-        print("\nGET recibido") --DEBUG
+        print("\nGET received") --DEBUG
         sendAPlistPage(conn)
     else
-        print("\nPOST recibido") --DEBUG
+        print("\nPOST received") --DEBUG
         local blank, plStart = string.find(payload, "\r\n\r\n");
         if(plStart  ==  nil) then
             return
@@ -59,17 +59,17 @@ function incomingConnection(conn, payload)
         payload = string.sub(payload, plStart+1)
         local args={}
         args.passwd=""
-        -- parseamos todos las variables POST en la tabla args[]
+        -- parse all POST variables of args[] table
         for k,v in string.gmatch(payload, "([^=&]*)=([^&]*)") do
             args[k]=url_decode(v)
         end
         if(args.ssid ~= nil and args.ssid ~= "") then
-            print("Nueva SSID: " .. args.ssid) --DEBUG
+            print("New SSID: " .. args.ssid) --DEBUG
             print("Password: " .. args.passwd) --DEBUG
             wifi.sta.config(args.ssid, args.passwd)
         end
         if(args.reboot ~= nil) then
-            print("Reiniciando") --DEBUG
+            print("Rebooting...") --DEBUG
             conn:send('HTTP/1.1 200 OK\n\n<!DOCTYPE HTML>\n<html>\n<head><meta content="text/html; charset=utf-8">\n<title>'.. args.ssid ..'</title></head>\n<body>\n\n<br/>Conectando a: '.. args.ssid ..'\n</body></html>')
             conn:close()
             node.restart()
